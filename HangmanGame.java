fc3f9016394e38deb01c38f3e224a792cf789ed2
Rodolfo Silva <126601333+Rodolfowolf@users.noreply.github.com>
2023-03-31 19:51:42 -0400

    Update HangmanGame.java

--- HangmanGame.java
+++ HangmanGame.java
@@ -1,6 +1,6 @@
 
 /**
- * Assignment 8 College Course objects working with bidimensional array and try catch.
+ * Final Project Hangmangame
  * @author Igor Guimaraes 200540189 - Navjot Kaur 200547472 - Yuvraj Singla - Rodolfo Carvalho 200536943
  * @date March 18, 2023
  * @time 09:00pm 
